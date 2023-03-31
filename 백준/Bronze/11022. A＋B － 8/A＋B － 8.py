a05f85a9a45d2f6a949f78152573cca03f15e263
KimTaeyun <91607691+hmnd1257@users.noreply.github.com>
2023-03-31 15:36:40 +0900

    [Bronze V] Title: A+B - 8, Time: 92 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,7 @@
+test_case = int(input())
+
+for i in range(1, test_case + 1):
+    a, b = map(int,input().split())
+    print("Case #",i,":", sep='',end=" ")
+    print(a, "+", b, "=",end=" ")
+    print(a+b)
