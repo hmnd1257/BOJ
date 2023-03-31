222552074e187edfec9a5579fd691beec498c45d
KimTaeyun <91607691+hmnd1257@users.noreply.github.com>
2023-03-31 15:18:38 +0900

    [Silver V] Title: 영화감독 숌, Time: 836 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,14 @@
+# 1436번 영화감독 숌
+n = int(input())
+
+num = 666
+cnt = 0
+
+while True:
+    if '666' in str(num):
+        cnt += 1
+
+    if n == cnt:
+        print(num)
+        break
+    num +=1
