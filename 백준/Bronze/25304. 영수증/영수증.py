c4acc499c6cac53b720a2dcbd1dcec3e94e8a98d
KimTaeyun <91607691+hmnd1257@users.noreply.github.com>
2023-04-01 18:21:00 +0900

    [Bronze V] Title: 영수증, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.py
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -5,7 +5,4 @@ total = 0
 for i in range(N):
     a, b = map(int, input().split())
     total += a*b
-if X == total:
-    print('Yes')
-else:
-    print('No')
+print('Yes') if X == total else print('No')
