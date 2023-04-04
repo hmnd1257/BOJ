9c85bcc9353762ec74cee35bbe934590667a166a
KimTaeyun <91607691+hmnd1257@users.noreply.github.com>
2023-04-04 21:54:43 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,25 +1,21 @@
-import sys
-
-data_dict = {"A+": 4.5, "A0": 4.0,
-             "B+": 3.5, "B0": 3.0,
-             "C+": 2.5, "C0": 2.0,
-             "D+": 1.5, "D0": 1.0,
-             "F": 0.0}
+grade_li = {'A+': 4.5,
+            'A0': 4.0,
+            'B+': 3.5,
+            'B0': 3.0,
+            'C+': 2.5,
+            'C0': 2.0,
+            'D+': 1.5,
+            'D0': 1.0,
+            'F': 0.0}
 
 result = 0
-total = 0
-
-for _ in range(0, 20, 1):
-    input_data = sys.stdin.readline().split()
-
-    if input_data[2] == "P":
+total_score = 0
+for i in range(20):
+    _, score ,grade = map(str, input().split())
+    if grade == 'P':
         continue
+    total_score += float(score)
+    result += float(score)*grade_li[grade]
 
-    total += float(input_data[1])
-    result += float(input_data[1]) * data_dict[input_data[2]]
-
-GPA = result / total
-
-conversion = format(GPA, '.6f')
-
-print(conversion)
+result = result/total_score
+print(f'{result:.6f}')
