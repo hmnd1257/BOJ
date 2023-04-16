1cf57fbd76cffbcb06f5968151b2c579c6448ef6
Taeyun Kim <91607691+hmnd1257@users.noreply.github.com>
2023-04-16 18:07:08 +0900

    Delete 2953번 나는 요리사다.py

--- 2953번 나는 요리사다.py
+++
@@ -1,9 +0,0 @@
-li = []
-for i in range(5):
-    a, b, c, d = map(int, input().split())
-    value = a+b+c+d
-
-    li.append(value)
-# print(li)
-print(li.index(max(li))+1, end=' ')
-print(max(li))
