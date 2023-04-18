a6c21aa2f013837e23f06adaa5f57610682b3a61
Taeyun Kim <91607691+hmnd1257@users.noreply.github.com>
2023-04-18 22:41:41 +0900

    Update update.py

--- update.py
+++ update.py
@@ -16,6 +16,8 @@ def main():
 
     for root, dirs, files in os.walk("."):
         dirs.sort()
+        content += f'zz{dirs}'
+        content += f'ss{files}'
         if root == '.':
             for dir in ('.git', '.github'):
                 try:
